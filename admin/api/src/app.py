27226d6ba05dfcadfd913ac18caea5bbd4225a0d
Joaquim Oliveira <joaquim.tiago1999@gmail.com>
2023-05-19 07:13:53 +0100

    changed cdn_api to url and redirect

--- admin/api/src/app.py
+++ admin/api/src/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify, request
+import socket
+from flask import Flask, jsonify, redirect, request
 from geopy.distance import geodesic as GD
 import json
 import requests
@@ -20,6 +21,13 @@ def stats_monitor():
     # add condition for when the file doesnt exist
     return json.load(open('../data/log_request.json'))
 
+
+def find_object_in_array(array, key, value):
+    for obj in array:
+        if key in obj and obj[key] == value:
+            return obj
+    return None
+
 # Returns geolocation info from ip addr
 def get_location(ip):
     ip_address = ip
@@ -35,18 +43,20 @@ def get_location(ip):
 
 # List of IPs to compare against
 ip_list = []
+ip_obj = []
 
 for i in json.load(open('../data/data.json')):
+    ip_obj.append(i)
     ip_list.append(get_location(i["ip"]))
 
 @app.route('/cdn/lb/ip', methods=['GET'])
-def closest_ip():
+@app.route('/cdn/lb/ip/<path:subpath>', methods=['GET'])
+def closest_ip(subpath=None):
     # Get the request IP
     req_ip = request.headers.get('X-Real-IP')
 
     # Get the geolocation of the request ip
     req_ip = get_location(req_ip)
-
     # Append request to the log file
     update_file(req_ip)
 
@@ -67,8 +77,19 @@ def closest_ip():
             closest_ip = ip
             min_distance = distance
 
+
+    found_object = find_object_in_array(ip_obj, "ip", closest_ip['ip'])
     # Return the closest IP as a JSON response
-    return jsonify({'ip': closest_ip["ip"],'name': str(closest_ip["region"]+" ("+closest_ip["country"]+")"), "dist": min_distance, "req_ip": req_ip["ip"]})
+    # return jsonify({'ip': closest_ip["ip"],'name': str(closest_ip["region"]+" ("+closest_ip["country"]+")"), "dist": min_distance, "req_ip": req_ip["ip"]})
+    
+    # Handle the route without additional content     
+    if subpath is None:   
+        # ...
+        return jsonify({'region': found_object["name"],'cdn_url': str("https://" + found_object["url"]),"request_ip": req_ip["ip"]})
+    
+    # Handle the route with additional content
+    else: 
+        return redirect(str("https://" + found_object["url"] + "/" + subpath), code=302)
 
 if __name__ == '__main__':
     app.run()
