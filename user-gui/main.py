0b435827caa63d30e241785880c2c931a74536bc
Joaquim Oliveira <joaquim.tiago1999@gmail.com>
2023-05-19 17:09:17 +0100

    initial client cli

--- user-gui/main.py
+++ user-gui/main.py
@@ -1,8 +1,7 @@
+import argparse
 import datetime
-import tkinter as tk
-from tkinter import filedialog
-from tkinter import ttk
 import json
+from urllib.parse import urljoin
 import requests
 import sys
 from urllib.request import urlopen
@@ -10,107 +9,121 @@ from tqdm import tqdm
 import math
 
 def convert_size(size_bytes):
-   if size_bytes == 0:
-       return "0B"
-   size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
-   i = int(math.floor(math.log(size_bytes, 1024)))
-   p = math.pow(1024, i)
-   s = round(size_bytes / p, 2)
-   return "%s %s" % (s, size_name[i])
+    if size_bytes == 0:
+        return "0B"
+    size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+    i = int(math.floor(math.log(size_bytes, 1024)))
+    p = math.pow(1024, i)
+    s = round(size_bytes / p, 2)
+    return "%s %s" % (s, size_name[i])
 
-class TextRedirector:
-    def __init__(self, text_widget):
-        self.text_widget = text_widget
 
-    def write(self, message):
-        self.text_widget.insert(tk.END, message)
-        self.text_widget.see(tk.END)  # Auto-scroll to the end
+def connect(cdn_url):
+    try:
+        response = requests.get(cdn_url)
+        response.raise_for_status()
+        json_data = response.json()
+        ip = requests.get('http://ipinfo.io/json').json()['ip']
+        print("External IP: ",ip)        
+        print("Connecting to: ", cdn_url)
+        print("Connected successfully!")
+        print("Region: ", json_data['region'])
+        print("CDN-Server-URL: ",json_data["cdn_url"])
+        return json_data
+    except requests.exceptions.RequestException as e:
+        print(f"Error connecting to the CDN service: {e}")
+        return None
 
-def fetch_data():
-    url = entry.get()
+
+def fetch_data(url):
+    if url.endswith("/"):
+        url = url[:-1]  # Remove trailing "/"
     try:
         response = requests.get(url)
         response.raise_for_status()
         data = response.json()
-        
-        my_game.delete(*my_game.get_children())  # Clear existing items
-        for item in data:            
+
+        for item in data:
             key = item["key"]
             last_modified = datetime.datetime.strptime(item["last_modified"], "%Y-%m-%dT%H:%M:%S.%fZ")
             size_in_bytes = int(item["size"])
-            my_game.insert('', tk.END, values=(key,last_modified,convert_size(size_in_bytes)))
-        log_message("Data fetched successfully.")
+            print(f"Key: {key}, Last Modified: {last_modified}, Size: {convert_size(size_in_bytes)}")
+        print("Data fetched successfully.")
+        return data
     except requests.exceptions.RequestException as e:
-        log_message(f"Error fetching data: {e}")
+        print(f"Error fetching data: {e}")
+        return
 
-def on_select(event):
-    selected_item = my_game.focus()
+def on_select(url, selected_item, text_output):
     if selected_item:
-        values = my_game.item(selected_item)['values']
+        values = selected_item['values']
         if values:
-            text_output.delete("1.0", tk.END)  # Clear existing text
-            text_output.insert(tk.END, f"URL: {entry.get()}/{values[0]}")
+            url_to_download = f"{url}/{values[0]}"
+            print(url_to_download)
+            text_output = url_to_download
 
-def download_file():
-    url = entry.get()
-    file_path = filedialog.asksaveasfilename(defaultextension=".json", filetypes=[("JSON files", "*.json")])
-    if file_path:
-        with urlopen(url) as response, open(file_path, 'wb') as file:
-            total_size = int(response.info().get('Content-Length', 0))
-            block_size = 1024
-            progress_bar['maximum'] = total_size
-            with tqdm(total=total_size, unit='B', unit_scale=True) as pbar:
-                while True:
-                    buffer = response.read(block_size)
-                    if not buffer:
-                        break
-                    file.write(buffer)
-                    progress_bar['value'] += len(buffer)
-                    pbar.update(len(buffer))
-            log_message("File downloaded successfully.")
+def download_file(url_to_download, file_path):
+    # download list of files
+    # fetched_keys = [item["key"] for item in fetch_data(url_to_download)]
+    # check if filename is in the list
+    # if file_path not in fetched_keys:
+        # print(f"Invalid filename. '{file_path}' is not a valid key in the fetched data.")
+        # return
+    
+    fullPath = urljoin(url_to_download, file_path)    
 
-def log_message(message):
-    console_output.insert(tk.END, message + "\n")
-    console_output.see(tk.END)  # Auto-scroll to the end
+    with requests.get(fullPath, stream=True) as response:
+        response.raise_for_status()
+        total_size = int(response.headers.get('Content-Length', 0))
+        block_size = 1024
+        progress_bar_maximum = total_size
 
-root = tk.Tk(className="CDN Download CLient")
-
-
-url_label = tk.Label(root, text="CDN-URL:")
-url_label.pack(pady=10)
-
-entry = tk.Entry(root,width=100)
-entry.insert(0, "http://127.0.0.1:8080/")
-entry.pack(pady=10)
-
-fetch_button = tk.Button(root, text="Fetch", command=fetch_data)
-fetch_button.pack()
+        with open(file_path, 'wb') as file, tqdm(total=total_size, unit='B', unit_scale=True) as pbar:
+            for buffer in response.iter_content(block_size):
+                if not buffer:
+                    break
+                file.write(buffer)
+                progress_bar_value = len(buffer)
+                pbar.update(len(buffer))
+        print("File downloaded successfully.")
 
 
-# Create a label for column headers
-my_game = ttk.Treeview(root, columns=('Name', 'TimeStamp', 'Size'))
-my_game.pack(pady=10)
+def main(cdn_url):
+    # entry = input("CDN-API-URL: ")
+    if cdn_url:
+        cdn_url_obj = connect(cdn_url)
+        # cdn_url_edge = cdn_url_obj["cdn_url"] 
+        cdn_url_edge = "https://proxy-service-eu-7nricgvmca-ew.a.run.app"
+    else:
+        print("CDN-API-URL is required.")
+        return
 
-for col in my_game['columns']:
-    my_game.heading(col, text=col)
-    my_game.column(col, width=400)
+    while True:
 
-my_game.bind('<<TreeviewSelect>>', on_select)
+        command = input("Enter a command (fetch, download, exit): ")
+        if command == "fetch":
+            # url = input("Enter the URL: ")
+            print("Fetching data from: ", cdn_url_edge)
+            fetch_data(cdn_url_edge)
+        elif command == "download":
+            # url_to_download = input("Enter the URL to download: ")
+            file_path = input("Enter the file name: ")
+            download_file(cdn_url_edge, file_path)
+        elif command == "exit":
+            break
+        else:
+            print("Invalid command.")
 
-text_output = tk.Text(root, height=5, width=40)
-text_output.pack(pady=10)
 
-download_button = tk.Button(root, text="Download", command=download_file)
-download_button.pack(pady=10)
 
-progress_bar = ttk.Progressbar(root, orient='horizontal', length=400, mode='determinate')
-progress_bar.pack(pady=10)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="CDN Download CLI")
+    parser.add_argument("--cdn-url", type=str, help="CDN API URL")
+    args = parser.parse_args()
 
-console_output = tk.Text(root, height=10)
-console_output.pack(pady=10)
+    cdn_url = args.cdn_url
 
-# Redirect stdout and stderr to the console output
-sys.stdout = TextRedirector(console_output)
-sys.stderr = TextRedirector(console_output)
-
-root.mainloop()
+    if cdn_url:
+        main(cdn_url)
+    else:
+        print("CDN-API-URL is required. Use --cdn-url to specify the URL.")
